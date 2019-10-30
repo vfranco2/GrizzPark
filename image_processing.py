adfe934cac13540024d65071354fa4d6ab4a8fb3
jmukes97 <jmukes@oakland.edu>
2019-10-30 01:13:13 -0400

    Add files via upload

---
+++ image_processing.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+image = Image.open('/home/jon/Pictures/testpic.jpg')
+box = (300, 0, 900, 700)
+image1 = image.crop(box)
+first_row_ = image1.rotate(-200)
+box4 = (100, 0, 500, 900)
+first_row = first_row_.crop(box4)
+first_row.save("firstrow.jpg")
+
+box2 = (900, -50, 1600, 800)
+image2 = image.crop(box2)
+second_row_ = image2.rotate(200)
+box3 = (175, 50, 600, 900)
+second_row = second_row_.crop(box3)
+second_row.save("secrow.jpg")
