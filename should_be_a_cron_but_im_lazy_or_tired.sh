859c6ebac3d4ccf15fa9d9ecb02ca6b491f8b268
Ubuntu <ubuntu@ip-172-26-10-83.us-east-2.compute.internal>
2019-10-31 04:06:26 +0000

    lazily added sh file to run all the ML stuff

---
+++ should_be_a_cron_but_im_lazy_or_tired.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true;do
+	  python3 image_processing.py;
+	  python3 test.py --image firstrow.jpg  --yolo ./yolo;
+	  python3 test.py --image secrow.jpg  --yolo ./yolo;
+	  sleep 10;
+done
