cb992b3380c0a9caf1308c4da43c4da750a14ab9
Vlad Franco <vladfranco2@gmail.com>
2019-10-31 04:28:07 -0400

    Linked app to database, merged db and google auth features

--- mobile/app/src/main/java/com/csi4999/grizzpark/MainActivity.java
+++ mobile/app/src/main/java/com/csi4999/grizzpark/MainActivity.java
@@ -1,7 +1,15 @@
 package com.csi4999.grizzpark;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,8 +19,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView[] textViewArray = new TextView[2];
+    RequestQueue queue;
+    String url ="http://3.133.151.170:4444/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +38,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        queue = Volley.newRequestQueue(getApplicationContext());
 
-    }
+        textViewArray[0] = findViewById(R.id.firstRow);
+        textViewArray[1] = findViewById(R.id.secondRow);
+        final int[] rowvar = new int [2];
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        final GradientDrawable[] gradientDrawable = new GradientDrawable[2];
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        gradientDrawable[0] = (GradientDrawable) textViewArray[0].getBackground().mutate();
+        gradientDrawable[1] = (GradientDrawable) textViewArray[1].getBackground().mutate();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-        return super.onOptionsItemSelected(item);
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONArray rowstate = response.getJSONArray("data");
+                            for (int i = 0; i<4; i++){
+                                int[] tempvar = new int [2];
+                                JSONObject obj = rowstate.getJSONObject(i);
+                                rowvar[i] = obj.getInt("numOfCars");
+                                int spots = 21-rowvar[i];
+                                textViewArray[i].setText(spots+" spot(s) available");
+//Integer.toString(rowvar[i])
+
+                                if (rowvar[i] < 14){gradientDrawable[i].setColor(Color.argb(60,177,255,177));}
+                                else if (rowvar[i] == 21){gradientDrawable[i].setColor(Color.argb(60,157,0,0));}
+                                else {gradientDrawable[i].setColor(Color.argb(60,245,245,10));}
+                            }
+                        }
+                        catch (JSONException e){
+                            System.out.println("off");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+        queue.add(jsonObjectRequest);
     }
 }
